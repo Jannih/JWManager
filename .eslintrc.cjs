833eb4a28f96c16da95fae4d8598761efc30db0d
Janni235 <janni.hares@colorando.de>
2024-11-30 18:22:04 +0000

    first commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,58 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['next/core-web-vitals'],
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:payload/recommended',
+    'prettier'
+  ],
   parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
     project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname,
   },
+  plugins: ['@typescript-eslint', 'payload'],
+  rules: {
+    'semi': ['off'],
+    'quotes': ['error', 'single'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-console': [
+      'error',
+      {
+        allow: ['info', 'warn', 'trace', 'error']
+      }
+    ]
+  },
+  overrides: [
+    {
+      files: ['./server/**/*.ts'],
+      rules: {
+        'no-console': [
+          'error',
+          {
+            allow: ['info', 'warn', 'trace', 'error']
+          }
+        ]
+      }
+    },
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:payload/recommended'
+      ],
+      rules: {
+        // Weitere spezifische Regeln für TypeScript
+      }
+    }
+  ]
 }
