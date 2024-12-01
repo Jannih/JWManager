599f21e767b9418777b250e6b68a04860f97db79
zehjotkah <111518041+zehjotkah@users.noreply.github.com>
2024-12-01 00:09:02 +0000

    fixed collection

--- payload.config.ts
+++ payload.config.ts
@@ -2,58 +2,14 @@ import sharp from 'sharp'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { buildConfig } from 'payload'
-import type { CollectionConfig } from 'payload/types'
-
-// First, let's define the collection configuration
-const LebenUndDienstCollection: CollectionConfig = {
-  slug: 'leben-und-dienst',
-  admin: {
-    useAsTitle: 'datum', // Use the date field as title
-  },
-  fields: [
-    {
-      name: 'datum',
-      type: 'date',
-      required: true,
-      admin: {
-        date: {
-          pickerAppearance: 'dayAndTime',
-          displayFormat: 'dd.MM.yyyy',
-        },
-      },
-    },
-    {
-      name: 'schaetzeAusGottesWort',
-      type: 'group',
-      label: 'SCHÄTZE AUS GOTTES WORT',
-      fields: [
-        {
-          name: 'aufgabe1',
-          type: 'text',
-          label: 'Aufgabe 1',
-        },
-      ],
-    },
-  ],
-}
+import { LebenUndDienst } from './collections/LebenUndDienst'
 
 export default buildConfig({
-  // If you'd like to use Rich Text, pass your editor here
   editor: lexicalEditor(),
-
-  // Define and configure your collections in this array
-  collections: [LebenUndDienstCollection],
-
-  // Your Payload secret - should be a complex and secure string, unguessable
+  collections: [LebenUndDienst],
   secret: process.env.PAYLOAD_SECRET || '',
-  // Whichever Database Adapter you're using should go here
-  // Mongoose is shown as an example, but you can also use Postgres
   db: mongooseAdapter({
     url: process.env.DATABASE_URI || '',
   }),
-  // If you want to resize images, crop, set focal point, etc.
-  // make sure to install it and pass it to the config.
-  // This is optional - if you don't need to do these things,
-  // you don't need it!
   sharp,
 })
---
+++ src/collections/LebenUndDienst.ts
@@ -0,0 +1,34 @@
+import { type CollectionConfig } from 'payload/types'
+
+export const LebenUndDienst: CollectionConfig = {
+  slug: 'leben-und-dienst',
+  admin: {
+    useAsTitle: 'datum',
+    defaultColumns: ['datum'],
+  },
+  fields: [
+    {
+      name: 'datum',
+      type: 'date',
+      required: true,
+      admin: {
+        date: {
+          pickerAppearance: 'dayAndTime',
+          displayFormat: 'dd.MM.yyyy',
+        },
+      },
+    },
+    {
+      name: 'schaetzeAusGottesWort',
+      type: 'group',
+      label: 'SCHÄTZE AUS GOTTES WORT',
+      fields: [
+        {
+          name: 'aufgabe1',
+          type: 'text',
+          label: 'Aufgabe 1',
+        },
+      ],
+    },
+  ],
+} 
